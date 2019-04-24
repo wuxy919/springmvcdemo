5054c356222f35fb3571a6cc03e04ec84688bf35
wuxy919 <2515948645@qq.com>
2019-04-24 09:59:50 +0800

    Spring MVC文件下载

--- src/main/java/com/wuxy/controller/FileController.java
+++ src/main/java/com/wuxy/controller/FileController.java
@@ -1,13 +1,17 @@
 package com.wuxy.controller;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +40,7 @@ public class FileController {
         return "upload";
     }
 
-    @PostMapping("uploads")
+    @PostMapping("/uploads")
     public String uploads(@RequestParam("img") MultipartFile[] multipartFiles,HttpServletRequest request){
         List<String> filePaths = new ArrayList<>();
         for (MultipartFile multipartFile:multipartFiles){
@@ -54,6 +58,34 @@ public class FileController {
         }
         request.setAttribute("filePaths",filePaths);
         return "uploads";
+    }
+
+    @GetMapping("/download")
+    public void downloadFile(String fileName, HttpServletRequest request, HttpServletResponse response) {
+        if (fileName != null) {
+            String path = request.getServletContext().getRealPath("file");
+            File file = new File(path, fileName);
+            OutputStream os = null;
+            if (file.exists()) {
+                // 设置下载文件
+                response.setContentType("application/force-download");
+                // 设置文件名
+                response.setHeader("content-Disposition", "attachment;filename=" + fileName);
+                try {
+                    os = response.getOutputStream();
+                    os.write(FileUtils.readFileToByteArray(file));
+                    os.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        os.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
 
---
+++ src/main/webapp/download.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2019/4/24
+  Time: 9:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <a href="download?fileName=图片1.png">下载图片</a>
+</body>
+</html>
